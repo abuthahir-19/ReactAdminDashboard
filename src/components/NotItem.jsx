b168b44f8463fc8ee7d501587934a5646c39236b
abuthahir-19 <abuthahirstar1@gmail.com>
2023-08-10 10:04:58 +0530

    updates

--- src/components/NotItem.jsx
+++ src/components/NotItem.jsx
@@ -3,7 +3,7 @@ const NotItem = ({ icon, content, mins }) => {
         <div className="relative py-2 px-4 mt-3">
             <div className="p-2 hover:bg-slate-200 rounded-md cursor-pointer">
                 <div className="flex w-full items-center justify-between">
-                    <p className="text-green-400">{icon}</p>
+                    <p className="text-green-400 text-lg">{icon}</p>
                     <p className="text-slate-900 ml-4 w-[85%] pr-5">{content}</p>
                 </div>
                 <p className="text-slate-400 flex justify-start mx-auto -mt-1 w-[70%]">{`${mins} ago`}</p>
